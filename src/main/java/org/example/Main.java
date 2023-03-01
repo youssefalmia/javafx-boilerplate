29dc3d28f3734647c7d7937ec0dd3dabe40b0a43
Youssef ALMIA <almia.youssef2@gmail.com>
2023-03-01 08:52:05 +0100

    simple fxml added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/interfaces/StudentView.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/interfaces/StudentSimpleView.fxml")));
         primaryStage.setTitle("Tuto app");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
---
+++ src/main/java/org/example/controllers/StudentSimpleViewController.java
@@ -0,0 +1,48 @@
+package org.example.controllers;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import org.example.model.*;
+
+/**
+ * @author Jozef
+ */
+public class StudentSimpleViewController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private ListView listView;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField ageField;
+
+    @FXML
+    private Text prof;
+
+    @FXML
+    private Text errorText;
+
+    @FXML
+    void addStudent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void browseFiles(ActionEvent event) {
+
+    }
+
+    @FXML
+    void findById(ActionEvent event) {
+
+    }
+
+}
